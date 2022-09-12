690ed3c44b2bbe324c2055dd1eab2ae2a83107dc
Jaime Calvo <info@digitalconcept.se>
2022-09-12 22:12:21 +0200

    refactor react, func instead of class

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # gansa
+
+Backend:
+
+dockerized wordpress.
+Git ignores:
+* everything in the root except the "wp-content" directory.
+* ignores everything in the "wp-content" directory, except:
+  "mu-plugins", "plugins", "themes" directory
+* Everything in db.
+
+Install following wp plugins:  
+* Custom Post Type UI
+* Advanced Custom Fields (ACF)
+* ACF to REST API
+
+fronted: 
+
+React, uses wp api to fetch post.
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,29 @@
-import React, { createContext, Component } from 'react';
-import Store from './Store';
-import PostsContainer from './containers/PostsContainer';
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>test</h1>
-        <Store>
-          <PostsContainer />
-        </Store>
-      </div>
-    );
-  }
-}
+import React, { useEffect, useState } from 'react';
 
-export default App;
+export default function Posts() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+      async function loadPosts() {
+          const response = await fetch('http://localhost:80/wp-json/wp/v2/posts');
+          if(!response.ok) {
+              // oups! something went wrong
+              return;
+          }
+          const posts = await response.json();
+          setPosts(posts);
+      }
+      loadPosts();
+ }, [])
+return (
+  <div>
+    {posts.map((post, index) => (
+    <div>
+      <div>
+        <div>{post.title.rendered} </div>
+        <div>{post.content.rendered} </div>
+      </div>
+    </div>
+   ))}
+  </div>
+);
+}
